607b59dc76923dbb3dacb3d0c4c06db191ba4591
adrian <adrian.x.marrero@nielsen.com>
2020-03-02 16:01:38 +0000

    commit

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,60 @@
+import { UsuarioModel } from './../../models/usuario.model';
+import { Component, OnInit } from '@angular/core';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+  recordarme = false;
+
+  constructor( private auth: AuthService, private router: Router ) { }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+    this.usuario.email = 'adrianmarmar@gmail.com';
+   }
+
+   onSubmit( form: NgForm  ) {
+
+    if ( form.invalid ) { return }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      text: 'Espere por favor',
+      icon: 'info',
+    });
+    Swal.showLoading();
+
+    this.auth.nuevoUsuario( this.usuario )
+      .subscribe( resp => {
+
+        console.log(resp);
+        Swal.close();
+
+        if ( this.recordarme ){
+          localStorage.setItem('email', this.usuario.email);
+        }
+
+        this.router.navigateByUrl('/home');
+
+      }, (err) => {
+        console.log(err.error.error.message);
+        Swal.fire({
+          text: err.error.error.message,
+          icon: 'error',
+        });
+      });
+
+   }
+
+
+}
